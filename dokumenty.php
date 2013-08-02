cd311f2cdfc28fa6d49a662a7a02d9aa29d9112a
anim4tor <jiri.klusak@gmail.com>
2013-08-02 12:15:41 +0200

    Sections added

---
+++ dokumenty.php
@@ -0,0 +1,19 @@
+<section class="image">
+  <img src="http://<?php echo $_SERVER['SERVER_NAME'].$folder;?>/img/documents.png">
+</section>
+<section id="content" class="container">
+  <section class="one title w-line">
+    <h2><span>Dokumenty</span> ke stažení.</h2>
+    <h3>Jsme transparentní. Naše dokumenty a osvědčení jsou volně ke stažení a tisku.</h3>
+  </section>
+  <section class="one content documents-list">
+    <ul>
+      <li><a href="docs/OR.pdf" class="icon icon-download"></a>Výpis z OR SPEDICE BRNO s.r.o.</li>
+      <li><a href="docs/Ziv.pdf" class="icon icon-download"></a>Živnostenský rejstřík SPEDICE BRNO s.r.o.</li>
+      <li><a href="docs/Osvědčení o DIČ DPH.pdf" class="icon icon-download"></a>Osvědčení o DIČ DPH</li>
+      <li><a href="docs/Koncesni listina SPEDICE BRNO s.r.o..pdf" class="icon icon-download"></a>Koncesni listina SPEDICE BRNO s.r.o.</li>
+      <li><a href="docs/Osvědčení o pojištění.pdf" class="icon icon-download"></a>Osvědčení o pojištění</li>
+    </ul>
+  </section>
+</section>
+        
---
+++ domu.php
@@ -0,0 +1,82 @@
+<section class="">
+  <div class="slider">
+    <ul class="slides">
+      <li class="slide slide_01">
+        <div class="slider-headline">
+          <h2>Komplexní řešení zprostředkování přepravy</h2>
+          <h3>vnitrostátních a mezinárodních zásilek.</h3>
+        </div>
+      </li>
+      <li class="slide slide_02">
+        <!-- <img src="img/truck.png" alt=""> -->
+        <div class="slider-headline">
+          <h2>Profesionální a kvalitní služby</h2>
+          <h3>v oblasti dopravy a logistiky.</h3>
+        </div>
+      </li>
+      <li class="slide slide_03">
+        <div class="slider-headline">
+          <h2>Spokojenost zákazníka je naší prioritou.</h2>
+          <h3>Výhodná a bezpečná přeprava.</h3>
+        </div>
+      </li>
+      <li class="slide slide_04">
+        <div class="slider-headline">
+          <h2>Přeprava zboží všeho druhu</h2>
+          <h3>dle požadavků zákazníka.</h3>
+        </div>
+      </li>
+    </ul>
+  </div>
+</section>
+<div class="horizontal-line"></div>
+<section id="content" class="container">
+  <section class="one title">
+    <h2>Vítejte na webu společnosti <span>Spedice Brno</span> s.r.o.</h2>
+    <h3>Jsme moderní a dynamická, dopravně-spediční společnost, která poskytuje komplexní řešení při přepravě zboží všeho druhu, ve vnitrostátní ale i mezinárodní oblasti dopravy.</h3>
+  </section>
+  <section class="one content alt map">
+    <ul class="home-menu">
+      <li class="one-fourth">
+        <a href="onas">
+          <div class="menu-icon">
+            <div class="icon-slide">
+              <div class="icon-info"></div><div class="icon-info"></div>
+            </div>
+          </div>
+          <h4>O nás</h4>
+        </a>
+      </li>
+      <li class="one-fourth">
+        <a href="sluzby">
+          <div class="menu-icon">
+            <div class="icon-slide">
+              <div class="icon-truck"></div><div class="icon-truck"></div>
+            </div>
+          </div>
+          <h4>Služby</h4>
+        </a>
+      </li>
+      <li class="one-fourth">
+        <a href="dokumenty">
+          <div class="menu-icon">
+            <div class="icon-slide">
+              <div class="icon-attachment"></div><div class="icon-attachment"></div>
+            </div>
+          </div>
+          <h4>Dokumenty</h4>
+        </a>
+      </li>
+      <li class="one-fourth last">
+        <a href="kontakt">
+          <div class="menu-icon">
+            <div class="icon-slide">
+              <div class="icon-mobile"></div><div class="icon-mobile"></div>
+            </div>
+          </div>
+          <h4>Kontakt</h4>
+        </a>
+      </li>
+    </ul>
+  </section>
+</section>
